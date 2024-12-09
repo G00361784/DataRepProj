6f163491b02556639a49bda07ec0a525b8c9b47b
unknown <joemccole12@gmail.com>
2024-12-09 19:28:33 +0000

    commit

---
+++ src/components/read.js
@@ -0,0 +1,27 @@
+
+import Movies from "./movies";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Read() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('<my_api_url>')
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>This is my Read Component.</h2>
+      <Movies myMovies={movies} />
+    </div>
+  );
+}
+
+export default Read;
